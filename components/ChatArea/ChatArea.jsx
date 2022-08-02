e5dd10cbd2e428cff5f5413b233398f701886aa1
chathurperera <chathuraperera007@gmail.com>
2022-08-02 20:03:35 +0530

    gif issue fix

--- components/ChatArea/ChatArea.jsx
+++ components/ChatArea/ChatArea.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import Image from "next/image";
 import styles from "../ChatArea/ChatArea.module.scss";
 import beginChat from "../../assets/images/icons8-chat.gif";
 import ChatLayout from "../ChatLayout/ChatLayout";
 const ChatArea = ({ selectedChat }) => {
   return (
     <div className={styles.chatArea}>
-      {selectedChat === "" ? 
+      {selectedChat === "" ? (
         <div className={styles.startChat}>
           <div className={styles.startChatImage}>
-            <img src={beginChat} alt="" />
+            <div className={styles.gifWrapper}>
+              <Image src={beginChat} alt="" />
+            </div>
             <div className={styles.startConversation}>Start Conversation </div>
           </div>
         </div>
-      : <ChatLayout selectedChat={selectedChat} /> }
+      ) : (
+        <ChatLayout selectedChat={selectedChat} />
+      )}
     </div>
   );
 };
